c28f99c42359a914e1d21eaf8baf48eb09ddc00e
Francisco Mesar <fmesar87@gmail.com>
2022-03-22 19:34:25 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+const cards = ["Guadalupe", "Ollie", "Aki"]
+function writeCards(cards){
+    const array=[];
+    for(let i = 0; i < cards.length; i++ )
+    {
+      array.push('Thank you, ' + cards[i] +', for the wonderful surprise gift!');
+    
+    
+    }
+ 
+    return array
+}
+
+
+
+function countDown(){
+    for(let i = 0; i < 11; i++ )
+
+    console.log(i);
+}
